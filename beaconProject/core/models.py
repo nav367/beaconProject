670b587b6470ab90f8c3464c204ff1f831d881cb
disha.patil <disha.patil@hashedin.com>
2017-07-28 22:49:06 +0530

    added templates and css

--- beaconProject/core/models.py
+++ beaconProject/core/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class MeetingRoom(models.Model):
+    name = models.CharField(max_length=20)
+    beacon_id = models.CharField(max_length=10)
+
+class Meeting(models.Model):
+    meeting_id = models.CharField(max_length=15)
+    user = models.ForeignKey(User)
+    meeting_room_id = models.ForeignKey(MeetingRoom)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+class Presence(models.Model):
+    user = models.ForeignKey(User)
+    meeting_room = models.ForeignKey(MeetingRoom)
+    timestamp = models.DateTimeField()
+    in_time = models.DateTimeField(null=True, blank=True)
+    out_time = models.DateTimeField(null=True, blank=True)
